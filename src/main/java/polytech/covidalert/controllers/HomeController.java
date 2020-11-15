f03bca966546b4ee01dcff0bae3e1d463509b028
Alexandre <alexandre.girbal.pro@gmail.com>
2020-11-15 18:06:41 +0100

    keycloak ok

--- src/main/java/polytech/covidalert/controllers/HomeController.java
+++ src/main/java/polytech/covidalert/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package polytech.covidalert.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +10,17 @@ import polytech.covidalert.models.User;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import java.util.Collections;
 
 @RestController
-@RequestMapping("/covidalert/api")
+@CrossOrigin
 public class HomeController {
     @Value("${app.version}")
     private String appVersion;
 
-    @GetMapping
-    public String versionApp() {
-        return appVersion;
+    @RequestMapping(value = "/covidalert/api", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object versionApp() {
+        return Collections.singletonMap("message", this.appVersion);
     }
 
 
