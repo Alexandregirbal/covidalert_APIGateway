3a6ccef8a8ca334000b064c5839ced30fd271a82
Alexandre <alexandre.girbal.pro@gmail.com>
2020-11-18 17:58:51 +0100

    cahnge kafkalocation class to fit topic consumer

--- src/main/java/polytech/covidalert/kafka/KafkaLocation.java
+++ src/main/java/polytech/covidalert/kafka/KafkaLocation.java
@@ -4,22 +4,27 @@ public class KafkaLocation {
     private String userEmail;
     private float longitude;
     private float latitude;
+    private int timestamp;
 
-    public KafkaLocation(String userEmail, float longitude, float latitude) {
+    public KafkaLocation(){}
+    public KafkaLocation(String userEmail, float longitude, float latitude, int timestamp) {
         this.userEmail = userEmail;
         this.longitude = longitude;
         this.latitude = latitude;
+        this.timestamp = timestamp;
     }
 
     public String getUserEmail() {
-        return userEmail;
+        return this.userEmail;
     }
 
     public float getLongitude() {
-        return longitude;
+        return this.longitude;
     }
 
     public float getLatitude() {
-        return latitude;
+        return this.latitude;
     }
+
+    public int getTimestamp() { return this.timestamp; }
 }
